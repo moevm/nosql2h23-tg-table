b0125e588dacb801276b20290a7a9acddb387080
DmitrySergeev02 <justbringit16@yandex.ru>
2023-11-26 22:06:04 +0300

    download graph button

--- admin_panel/src/components/dialogs/plot_dialog.js
+++ admin_panel/src/components/dialogs/plot_dialog.js
@@ -4,6 +4,20 @@ import Graph from "../graph";
 import login from "../login";
 
 const PlotDialog = (props) => {
+    const downloadGraph = async ()=>{
+        const data = requests.dates.map((e,ind)=> {
+            return `${formatDate(e)} : ${requests.requestCount[ind]}`
+        })
+        const blob = new Blob([data.join('\n')],{type : 'text/plain'})
+        const a = document.createElement('a');
+        a.download = `graph.txt`
+        a.href = URL.createObjectURL(blob);
+        a.addEventListener('click', (e) => {
+            setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
+        });
+        a.click();
+    }
+
     const formatDate = (date)=>{
         return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
     }
@@ -19,7 +33,7 @@ const PlotDialog = (props) => {
         })
         const newRequests = {
             dates: dates,
-            requestCount : dates.map(item=>(reqs.filter(e=>e.timestamp.getTime()===item.getTime())).length)
+            requestCount: dates.map(item => (reqs.filter(e => e.timestamp.getTime() === item.getTime())).length)
         }
         return newRequests;
     }
@@ -48,6 +62,7 @@ const PlotDialog = (props) => {
                     <button
                         className='defaultButton'
                         style={{marginTop: 15, marginLeft:"auto", fontSize: 18, minWidth: 100}}
+                        onClick={downloadGraph}
                     >
                         Сохранить
                     </button>
