4b5d20084bb4293eff59b582b3fb049e5ebad7b0
lizavetaChe <elizaveta.cheg@gmail.com>
2023-12-08 22:28:15 +0300

    docker - done !!!

--- server/src/main.py
+++ server/src/main.py
@@ -8,7 +8,6 @@ from login_routes import router as login_router
 from spreadsheets_routes import router as spreadsheets_router
 from requests_routes import router as requests_router
 
-
 app = FastAPI()
 
 origins = [
@@ -20,8 +19,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"]
 )
 
 
